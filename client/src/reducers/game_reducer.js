7aebf41ddd5ba7719b738ee66f3480944e3de041
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-06-21 13:15:28 -0700

    Added firebase setup

--- client/src/reducers/game_reducer.js
+++ client/src/reducers/game_reducer.js
@@ -1,6 +1,5 @@
-// import { BOARD } from './game_data';
 import Game from '../game_logic/game';
-import { PLACE_PIECE } from '../actions/types'; 
+import types from '../actions/types'; 
 
 const DEFAULT_STATE = {
     game: (new Game([1, 2])).events[0].payload.game
@@ -8,7 +7,7 @@ const DEFAULT_STATE = {
 
 export default function(state = DEFAULT_STATE, action){
     switch(action.type){
-        case PLACE_PIECE:
+        case types.PLACE_PIECE:
             state.game.place(state.game.players.current, action.payload.r, action.payload.c);
             return { ...state};
         default:
