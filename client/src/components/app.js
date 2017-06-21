f0e3d8f09a1513c946b42a225eb48f2e679a78a2
Andy <aong93@gmail.com>
2017-06-21 15:38:48 -0700

    Created sign up form with state

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,10 +3,12 @@ import './app.css';
 import Board from './board';
 import ScoreBoard from './score_board';
 import Login from './login';
+import Signup from './signup';
 
 const App = () => (
     <div>
         <Login/>
+        <Signup/>
         <h1 className="text-center">Othello</h1>
         <ScoreBoard/>
         <Board/>
---
+++ client/src/components/signup.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            form: {
+                username: '',
+                email: '',
+                password: '',
+                verifyPw: ''
+            }
+        }
+    }
+
+    handleInputChange(changeEvent, inputType){
+        const {value} = changeEvent.target;
+        const {form} = this.state;
+        let newFormState = {};
+        switch(inputType){
+            case 'username':
+                newFormState = {...form, username: value};
+                break;
+            case 'email':
+                newFormState = {...form, email: value};
+                break;
+            case 'password':
+                newFormState = {...form, password: value};
+                break;
+            case 'verifyPw':
+                newFormState = {...form, verifyPw: value};
+                break;
+            default:
+                return;
+        }
+        this.setState({form: newFormState});
+    }
+
+    handleFormSubmit(formEvent){
+        console.log(this.state.form);
+        this.setState({
+            form: {
+                username: '',
+                email: '',
+                password: '',
+                verifyPw: ''
+            }
+        });
+        console.log(this.state);
+    }
+    render(){
+        return(
+            <div className="signup">
+                <input type="text"
+                    placeholder="username"
+                    value={this.state.form.username}
+                    onChange={(event)=>this.handleInputChange(event, 'username')}/>
+                <input type="email"
+                    placeholder="email" 
+                    value={this.state.form.email} 
+                    onChange={(event)=>this.handleInputChange(event, 'email')}/>
+                <input type="password"
+                    placeholder="password" 
+                    value={this.state.form.password}
+                    onChange={(event)=>this.handleInputChange(event, 'password')}/>
+                <input type="password"
+                    placeholder="retype password"
+                    value={this.state.form.verifyPw}
+                    onChange={(event)=>this.handleInputChange(event, 'verifyPw')}/>
+                <button onClick={(event)=>this.handleFormSubmit(event)}>SIGN UP DOOD</button>
+            </div>
+        )
+    }
+}
+
+export default Signup;
