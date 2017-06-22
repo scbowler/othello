7f3195b5f0daaa7db3ab5d55d1b0e8bc73bee30a
collette-tamez <cttmz4@gmail.com>
2017-06-21 18:44:44 -0700

    restructured files, loaded custom fonts, new logo

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,15 +3,16 @@ import './app.css';
 import Board from './board';
 import ScoreBoard from './score_board';
 import User from './user';
+import Logo from './images/invadersim_logo.png';
 
 const App = () => (
     <div>
-        <User/>
-        <h1 className="text-center">Othello</h1>
+        <img src={Logo} alt="issa logo" className="logo"/>
         <div className="container-fluid">
             <div className="center">
                 <Board/>
                 <ScoreBoard/>
+                <User/>
             </div>
         </div>
     </div>
