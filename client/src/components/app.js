0ba66883b927e1b7e8fd3a42ed363f6ed4ea08f3
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-06-21 17:59:11 -0700

    Create user and Sign in complete

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './app.css';
-// import Board from './board';
-// import ScoreBoard from './score_board';
+import Board from './board';
+import ScoreBoard from './score_board';
 import User from './user';
-import Login from './login';
-import Signup from './signup';
 
 const App = () => (
     <div>
         <User/>
         <h1 className="text-center">Othello</h1>
-        {/*<ScoreBoard/>
-        <Board/>*/}
+        <ScoreBoard/>
+        <Board/>
     </div>
 );
 
